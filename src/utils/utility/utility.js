4912fa9166816eb8a95986188184eabe1921a9da
CodeReview By Rony <freelancinghelpdesk1@gmail.com>
2025-12-18 18:28:29 +0600

    update route match and params function

--- src/utils/utility/utility.js
+++ src/utils/utility/utility.js
@@ -1,18 +1,35 @@
-export const matchRouteAndParamsFun = (routePath, reqPath) => {
-  const routeParts = routePath.split("/").fillter(Boolean);
-  const reqParts = reqPath.split("/").fillter(Boolean);
+// export const matchRouteAndParamsFun = (routePath, reqPath) => {
+//   const routeParts = routePath.replace(/\/+$/, "").split("/").filter(Boolean);
+//   const reqParts = reqPath.replace(/\/+$/, "").split("/").filter(Boolean);
 
-  if (reqParts.length !== routeParts) return null;
+//   if (reqParts.length !== routeParts) return null;
+
+//   const params = {};
+
+//   for (let i = 0; i < routeParts.length; i++) {
+//     if (routeParts[i].startsWith(":")) {
+//       const key = routeParts[i].slice(1);
+//       params[key] = reqParts[i];
+//     } else if (routeParts[i].trim() !== reqParts[i].trim()) {
+//       return null;
+//     }
+//   }
+//   return params;
+// };
+
+export function matchRouteAndParamsFun(routePath, requestPath) {
+  const routeParts = routePath.replace(/\/+$/, "").split("/").filter(Boolean);
+  const reqParts = requestPath.replace(/\/+$/, "").split("/").filter(Boolean);
+
+  if (routeParts.length !== reqParts.length) return null;
 
   const params = {};
-
   for (let i = 0; i < routeParts.length; i++) {
     if (routeParts[i].startsWith(":")) {
-      const key = routeParts[i].slice(1);
-      params[key] = reqParts[i];
-    } else if (routeParts[i] !== reqParts[i]) {
+      params[routeParts[i].slice(1)] = reqParts[i];
+    } else if (routeParts[i].trim() !== reqParts[i].trim()) {
       return null;
     }
   }
   return params;
-};
+}
