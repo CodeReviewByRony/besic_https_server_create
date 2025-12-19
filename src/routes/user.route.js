8ec061d551aec92658aa209cb63c0d9eac6b78db
CodeReview By Rony <freelancinghelpdesk1@gmail.com>
2025-12-19 14:28:44 +0600

    user route update user log out logic with middlwere function done

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,4 +1,5 @@
 import { login, newUserCreate } from "../controllers/user.controller.js";
+import { authMiddlwere } from "../middlwere/auth.middlwere.js";
 
 export const userRoute = [
   {
@@ -10,5 +11,11 @@ export const userRoute = [
     method: "POST",
     path: "/user/login",
     handler: login,
+  },
+  {
+    method: "POST",
+    path: "/user/log-out",
+    handler: logout,
+    middlwere: authMiddlwere,
   },
 ];
